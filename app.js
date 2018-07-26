a94871f8b4f98dde6c2e0bfd242d6eab246562b6
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-07-26 02:22:54 -0400

    modificando coordenadas del mapa, para santiago de chile

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var santiagoDeChile = {lat: -33.449495, lng: -70.670096};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+      document.getElementById('map'), {zoom: 4, center:santiagoDeChile });
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = new google.maps.Marker({position: santiagoDeChile, map: map});
 }
